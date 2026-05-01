abfd646c9c8fd479a0060fbd8429aa521afb8a91
Douglas Tawile(Code Whisperer) <49791450+douglastawile@users.noreply.github.com>
2026-05-01 08:31:59 +0000

    icons and favicons updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,19 +29,19 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "/placeholder-logo.png",
         media: "(prefers-color-scheme: light)",
       },
       {
-        url: "/icon-dark-32x32.png",
+        url: "/placeholder-logo.png",
         media: "(prefers-color-scheme: dark)",
       },
       {
-        url: "/icon.svg",
+        url: "/placeholder-logo.svg",
         type: "image/svg+xml",
       },
     ],
-    apple: "/apple-icon.png",
+    apple: "/placeholder-logo.png",
   },
   openGraph: {
     title: "Douglas Tawile Coffee Shop",
